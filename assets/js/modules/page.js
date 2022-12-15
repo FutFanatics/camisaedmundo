80e4c88396ad64a3c5607ea85fd452025ec557f8
Manoela Frattini <manoelamoreno78@gmail.com>
2022-12-15 12:14:00 -0300

    describe

--- assets/js/modules/page.js
+++ assets/js/modules/page.js
@@ -5,7 +5,7 @@
 	// 	el: '.copa-do-mundo',
 	// 	lazyClass: 'lazy',
 	// }).fire();
-	var containerTech = $('.tech__slick')	
+	var containerTech = $('.describe__slick')	
 	var containerWarm = $('.warm__slick')
 	var containerBanner = $('.slick__banner')
 
@@ -17,43 +17,16 @@
         arrows: false,
         dots: false,
         adaptiveHeight: false,
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
-    });
-	
-	containerWarm.slick({
-		autoplay: false,
-        autoplaySpeed: 4000,
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        arrows: false,
-        dots: false,
-        adaptiveHeight: false,
-        slidesToShow: 5,
 
 		responsive: [{
 			breakpoint: 768,
 			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1,
-				centerMode: true,
-				adaptiveHeight: false,
-                vertical: false,
-                verticalSwiping: false,
-                
 			}
 		}]
-	});
-	containerBanner.slick({
-		autoplay: false,
-        autoplaySpeed: 4000,
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        arrows: false,
-        dots: false,
-        adaptiveHeight: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    });	
+    });
 })(jQuery);
 
