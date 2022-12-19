46ca5dc98e26b3eb989ea7d75d7e2faabcc83bf8
Manoela Frattini <manoelamoreno78@gmail.com>
2022-12-19 13:50:58 -0300

    home

--- assets/js/modules/page.js
+++ assets/js/modules/page.js
@@ -7,6 +7,7 @@
 	// }).fire();
 	var containerTech = $('.describe__slick')	
 	var containerShirt = $('.slick__shirt')	
+	var containerFooter = $('.slick__footer')	
 	var containerWarm = $('.warm__slick')
 	var containerBanner = $('.slick__banner')
 
@@ -38,6 +39,25 @@
         dots: false,
         adaptiveHeight: false,
         slidesToShow: 1,
+        slidesToScroll: 1,
+
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		}]
+    });
+	containerFooter.slick({
+		autoplay: false,
+        autoplaySpeed: 4000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        arrows: false,
+        dots: false,
+        adaptiveHeight: false,
+        slidesToShow: 4,
         slidesToScroll: 1,
 
 		responsive: [{
